da1648fbea32523b76bdd950a472257f327a71d6
Hans Pabst <hans.pabst@intel.com>
2020-01-16 17:05:32 +0100

    Moved timer-service into generator library (where the cpuid facility is already). This is because timer now needs cpuid to determine if TSC is running at a constant rate. In turn, this is necessary to check for certain virtualized environments where the timer can vary wildly.

--- src/libxs_timer.c
+++ src/libxs_timer.c
@@ -9,7 +9,8 @@
 ******************************************************************************/
 #include <libxs_timer.h>
 #include <libxs_intrinsics_x86.h>
-#include "libxs_main.h"
+#include <libxs_generator.h>
+#include <libxs_sync.h>
 
 #if defined(LIBXS_OFFLOAD_TARGET)
 # pragma offload_attribute(push,target(LIBXS_OFFLOAD_TARGET))
@@ -19,6 +20,9 @@
 #elif defined(__GNUC__) || defined(__PGI) || defined(_CRAYC)
 # include <sys/time.h>
 # include <time.h>
+#endif
+#if !defined(NDEBUG)
+# include <stdio.h>
 #endif
 #if defined(LIBXS_OFFLOAD_TARGET)
 # pragma offload_attribute(pop)
@@ -65,7 +69,8 @@ LIBXS_API_INTERN libxs_timer_tickint libxs_timer_tick_rtc(int* tsc)
   result = 1000000ULL * t.tv_sec + t.tv_usec;
 #endif
 #if defined(LIBXS_TIMER_RDTSC)
-  if (NULL != tsc) *tsc = 1;
+  if (0 == libxs_ninit) libxs_cpuid();
+  if (NULL != tsc) *tsc = (0 <= libxs_timer_scale ? 1 : 0);
 #else
   if (NULL != tsc) *tsc = 0;
 #endif
@@ -90,7 +95,6 @@ LIBXS_API double libxs_timer_duration(libxs_timer_tickint tick0, libxs_timer_tic
 {
   double result = (double)LIBXS_DELTA(tick0, tick1);
 #if defined(LIBXS_TIMER_RDTSC)
-  LIBXS_INIT
   if (0 < libxs_timer_scale) {
     result *= libxs_timer_scale;
   }
