89fb24cc68daf6130b695d0034e1e0848eb8688c
Hans Pabst <hans.pabst@intel.com>
2020-03-15 19:28:58 -0700

    added initial implementation of SGD

---
+++ src/libxs_dnn_optimizer_sgd.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+* Copyright (c) 2009-2026 Hans Pabst                                          *
+* Copyright (c) 2009-2026 Intel Corporation                                   *
+* This file is part of the LIBXS library.                                     *
+*                                                                             *
+* For information on the license, see the LICENSE file.                       *
+* Further information: https://github.com/hfp/libxs/                              *
+* SPDX-License-Identifier: BSD-3-Clause                                       *
+******************************************************************************/
+#ifndef LIBXS_DNN_OPTIMIZER_SGD_H
+#define LIBXS_DNN_OPTIMIZER_SGD_H
+
+#include <libxs_dnn_optimizer.h>
+
+LIBXS_API_INTERN libxs_dnn_err_t libxs_dnn_optimizer_sgd_st(libxs_dnn_optimizer* handle, int start_thread, int tid);
+
+#endif /* LIBXS_DNN_OPTIMIZER_SGD_H */
