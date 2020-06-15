8dcfd2515948bfca47e127e6927cdfa6951ef5d7
Hans Pabst <hans.pabst@intel.com>
2020-06-15 14:16:55 +0100

    Issue #385: Use L1 selector operands to increase NNZ for csr_asparse_reg

--- src/libxs_main.h
+++ src/libxs_main.h
@@ -729,6 +729,7 @@ struct LIBXS_RETARGETABLE libxs_dfsspmdm {
   int ldb;
   int ldc;
   int N_chunksize;
+  unsigned int* permute_operands;
   double* a_dense;
   libxs_dmmfunction kernel;
 };
@@ -740,6 +741,7 @@ struct LIBXS_RETARGETABLE libxs_sfsspmdm {
   int ldb;
   int ldc;
   int N_chunksize;
+  unsigned int* permute_operands;
   float* a_dense;
   libxs_smmfunction kernel;
 };
