ddbab0b5cdda7fe0cd786dc6a656c58409934073
Hans Pabst <hans.pabst@intel.com>
2022-08-17 13:01:16 +0200

    Changed argument order (libxs_batch/omp). This break compatibility with DBCSR's OpenCL BE. Remains compatible with CP2K/DBCSR thanks to calling libxs_batch with named arguments (Fortran).

--- samples/utilities/smmbench/specialized.cpp
+++ samples/utilities/smmbench/specialized.cpp
@@ -182,9 +182,9 @@ int main(int argc, char* argv[])
         const libxs_blasint ptrsize = sizeof(void*);
         const unsigned long long start = libxs_timer_tick();
         for (libxs_blasint r = 0; r < nrepeat; ++r) {
-          USEOMP(libxs_gemm_batch)(LIBXS_DATATYPE(ITYPE), LIBXS_DATATYPE(OTYPE), &transa, &transb,
-            m, n, k, &alpha, &a_array[0], &lda, &b_array[0], &ldb, &beta, &c_array[0], &ldc,
-            0/*index_base*/, 0/*index_stride*/, &ptrsize, &ptrsize, &ptrsize, s);
+          USEOMP(libxs_gemm_batch)(LIBXS_DATATYPE(ITYPE), LIBXS_DATATYPE(OTYPE), &transa, &transb, m, n, k,
+            &alpha, &a_array[0], &lda, &ptrsize, &b_array[0], &ldb, &ptrsize, &beta, &c_array[0], &ldc, &ptrsize,
+            0/*index_stride*/, 0/*index_base*/, s);
         }
         const unsigned long long ncycles = libxs_timer_ncycles(start, libxs_timer_tick());
         const double duration = libxs_timer_duration(0, ncycles) / nrepeat;
@@ -249,9 +249,9 @@ int main(int argc, char* argv[])
         const libxs_blasint ptrsize = sizeof(void*);
         const unsigned long long start = libxs_timer_tick();
         for (libxs_blasint r = 0; r < nrepeat; ++r) {
-          USEOMP(libxs_gemm_batch)(LIBXS_DATATYPE(ITYPE), LIBXS_DATATYPE(OTYPE), &transa, &transb,
-            m, n, k, &alpha, &a_array[0], &lda, &b_array[0], &ldb, &beta, &c_array[0], &ldc,
-            0/*index_base*/, 0/*index_stride*/, &ptrsize, &ptrsize, &ptrsize, s);
+          USEOMP(libxs_gemm_batch)(LIBXS_DATATYPE(ITYPE), LIBXS_DATATYPE(OTYPE), &transa, &transb, m, n, k,
+            &alpha, &a_array[0], &lda, &ptrsize, &b_array[0], &ldb, &ptrsize, &beta, &c_array[0], &ldc, &ptrsize,
+            0/*index_stride*/, 0/*index_base*/, s);
         }
         const unsigned long long ncycles = libxs_timer_ncycles(start, libxs_timer_tick());
         const double duration = libxs_timer_duration(0, ncycles) / nrepeat;
@@ -316,9 +316,9 @@ int main(int argc, char* argv[])
         const libxs_blasint ptrsize = sizeof(void*);
         const unsigned long long start = libxs_timer_tick();
         for (libxs_blasint r = 0; r < nrepeat; ++r) {
-          USEOMP(libxs_gemm_batch)(LIBXS_DATATYPE(ITYPE), LIBXS_DATATYPE(OTYPE), &transa, &transb,
-            m, n, k, &alpha, &a_array[0], &lda, &b_array[0], &ldb, &beta, &c_array[0], &ldc,
-            0/*index_base*/, 0/*index_stride*/, &ptrsize, &ptrsize, &ptrsize, s);
+          USEOMP(libxs_gemm_batch)(LIBXS_DATATYPE(ITYPE), LIBXS_DATATYPE(OTYPE), &transa, &transb, m, n, k,
+            &alpha, &a_array[0], &lda, &ptrsize, &b_array[0], &ldb, &ptrsize, &beta, &c_array[0], &ldc, &ptrsize,
+            0/*index_stride*/, 0/*index_base*/, s);
         }
         const unsigned long long ncycles = libxs_timer_ncycles(start, libxs_timer_tick());
         const double duration = libxs_timer_duration(0, ncycles) / nrepeat;
@@ -396,9 +396,9 @@ int main(int argc, char* argv[])
         const libxs_blasint ptrsize = sizeof(void*);
         const unsigned long long start = libxs_timer_tick();
         for (libxs_blasint r = 0; r < nrepeat; ++r) {
-          USEOMP(libxs_gemm_batch)(LIBXS_DATATYPE(ITYPE), LIBXS_DATATYPE(OTYPE), &transa, &transb,
-            m, n, k, &alpha, &a_array[0], &lda, &b_array[0], &ldb, &beta, &c_array[0], &ldc,
-            0/*index_base*/, 0/*index_stride*/, &ptrsize, &ptrsize, &ptrsize, 0 == check ? -s : s);
+          USEOMP(libxs_gemm_batch)(LIBXS_DATATYPE(ITYPE), LIBXS_DATATYPE(OTYPE), &transa, &transb, m, n, k,
+            &alpha, &a_array[0], &lda, &ptrsize, &b_array[0], &ldb, &ptrsize, &beta, &c_array[0], &ldc, &ptrsize,
+            0/*index_stride*/, 0/*index_base*/, 0 == check ? -s : s);
         }
         const unsigned long long ncycles = libxs_timer_ncycles(start, libxs_timer_tick());
         const double duration = libxs_timer_duration(0, ncycles) / nrepeat;
@@ -467,9 +467,9 @@ int main(int argc, char* argv[])
         const libxs_blasint ptrsize = sizeof(void*);
         const unsigned long long start = libxs_timer_tick();
         for (libxs_blasint r = 0; r < nrepeat; ++r) {
-          USEOMP(libxs_gemm_batch)(LIBXS_DATATYPE(ITYPE), LIBXS_DATATYPE(OTYPE), &transa, &transb,
-            m, n, k, &alpha, &a_array[0], &lda, &b_array[0], &ldb, &beta, &c_array[0], &ldc,
-            0/*index_base*/, 0/*index_stride*/, &ptrsize, &ptrsize, &ptrsize, 0 == check ? -s : s);
+          USEOMP(libxs_gemm_batch)(LIBXS_DATATYPE(ITYPE), LIBXS_DATATYPE(OTYPE), &transa, &transb, m, n, k,
+            &alpha, &a_array[0], &lda, &ptrsize, &b_array[0], &ldb, &ptrsize, &beta, &c_array[0], &ldc, &ptrsize,
+            0/*index_stride*/, 0/*index_base*/, 0 == check ? -s : s);
         }
         const unsigned long long ncycles = libxs_timer_ncycles(start, libxs_timer_tick());
         const double duration = libxs_timer_duration(0, ncycles) / nrepeat;
