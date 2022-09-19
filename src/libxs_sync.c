2c829d85ea82ea9be5a2150cb7b75ecd0d95d080
Hans Pabst <hans.pabst@intel.com>
2022-09-19 15:34:44 +0200

    Prefer empty block over semicolon in case of while loop with an empty body.

--- src/libxs_sync.c
+++ src/libxs_sync.c
@@ -145,7 +145,7 @@ LIBXS_API void libxs_barrier_init(libxs_barrier* barrier, int tid)
       barrier->init_done = 1; /* true */
     }
     else {
-      while (0/*false*/ == barrier->init_done);
+      while (0/*false*/ == barrier->init_done) {} /* empty block instead of semicolon */
     }
 
     /* set required per-thread information */
@@ -170,7 +170,7 @@ LIBXS_API void libxs_barrier_init(libxs_barrier* barrier, int tid)
       barrier->init_done = 2;
     }
     else {
-      while (2 != barrier->init_done);
+      while (2 != barrier->init_done) {} /* empty block instead of semicolon */
     }
   }
 #endif
