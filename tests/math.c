libxsmm/libxsmm

Backported non-breaking fixes and updates from main branch
* General
  - Limit warning about LIBXSMM_TARGET beyond CPUID to NDEBUG (verbose output), and avoid calling CPUID
    at all if LIBXSMM_TARGET is given. This regime allows easier debugging under ARM (no SIGILL).
  - Made libxsmm_get_gemm_typename use LIBXSMM_API_INLINE requiring no prototype,
    and effectively making the function private/internal (not part of the API).
  - Capture shared library file-pattern for all supported platforms (#783).
  - MacOS: account for non-Clang/Apple compiler on newer OS/SDK (Makefile.inc).
  - Fixed testing for existing directory in case of empty input (Makefile.inc).
  - Makefile.inc: disabled forcing coherent flags between Fortran and C/C++.
  - Improved mixed compiler setup under MacOS (LDFLAGS for libSystem).
  - Support generating test-scripts in an out-of-tree fashion (xgemm).
  - Consolidated handling errors in libxsmm_malloc_xattrib (#775).
  - Fixed code/format conformance (libxsmm_get_ulp_precision).
  - Attempted to improve targeting AArch64 under Windows.
  - Adjusted API of libxsmm_shuffle (libxsmm_memory.h).
  - Disabled CHECK_REALLOC under Windows.
  - More accurate LIBXSMM_PAGE_MINSIZE.
  - Code cleanup (libxsmm_coprime2).
  - Fixed typos in code comments.

* Eltwise and Equation
  - Modernized equation tests to rely on libxsmm_matdiff_epsilon (LIBXSMM_MATDIFF-tool, and more robust norm, etc).
  - Modernized eltwise to rely on libxsmm_matdiff_epsilon (LIBXSMM_MATDIFF-tool, etc).
  - Ensure all temporary files are removed at exit (generate_unary_transform_test_scripts.sh);
    there cannot be multiple EXIT-traps per script.
  - Support generating test-scripts in an out-of-tree fashion.
  - Adjusted error margins for eltwise tests according to new error measure.
  - Fixed opreduce script generating test-scripts (chmod executable flag).
  - Avoid potential division by zero (calculated speedup).

* Performance Report
  - Revised handling retention to allow pruni

tests/math.c
@@ -265,6 +265,7 @@ int main(int argc, char* argv[])
     const int n = sizeof(inp) / sizeof(*inp);
     for (i = 0; i < n; ++i) {
       if (LIBXSMM_UPF(inp[i], 3, 100) != out[i]) exit(EXIT_FAILURE);
+      if (LIBXSMM_UPF(inp[i], -100, 100) != 0) exit(EXIT_FAILURE);
     }
   }
 

tests/memory.c
@@ -117,6 +117,29 @@ int main(int argc, char* argv[])
   }
 
   if (EXIT_SUCCESS == result) { /* check libxsmm_shuffle */
+    char a[sizeof(init)] = { 0 }, b[sizeof(init)] = { 0 };
+    const size_t size = sizeof(init);
+    memcpy(a, init, size);
+    LIBXSMM_EXPECT(EXIT_SUCCESS == libxsmm_shuffle(a, 1, size - 1, NULL, NULL));
+    LIBXSMM_EXPECT(EXIT_SUCCESS == libxsmm_shuffle2(b, init, 1, size - 1, NULL, NULL));
+    if (0 == strcmp(a, b)) {
+      const size_t r = libxsmm_unshuffle(size - 1, NULL);
+      size_t i = 0;
+      for (; i < r; ++i) {
+        libxsmm_shuffle(a, 1, size - 1, NULL, NULL);
+      }
+      if (0 != strcmp(a, init)) {
+        FPRINTF(stderr, "libxsmm_shuffle: data not restored!\n");
+        result = EXIT_FAILURE;
+      }
+    }
+    else {
+      FPRINTF(stderr, "libxsmm_shuffle: result does not match libxsmm_shuffle2!\n");
+      result = EXIT_FAILURE;
+    }
+  }
+
+  if (EXIT_SUCCESS == result) { /* check libxsmm_shuffle2 */
     char a[sizeof(init)], b[sizeof(init)];
     const size_t size = sizeof(init);
     size_t s = 0, i;
@@ -127,35 +150,35 @@ int main(int argc, char* argv[])
         const size_t r = libxsmm_unshuffle(s, &shuffle);
         int cmp;
         memset(a, 0, size); /* clear */
-        libxsmm_shuffle(a, init, 1, s, &shuffle, NULL);
+        LIBXSMM_EXPECT(EXIT_SUCCESS == libxsmm_shuffle2(a, init, 1, s, &shuffle, NULL));
         cmp = memcmp(a, init, s);
         if ((1 >= s || 0 == cmp) && (1 < s || 0 != cmp)) {
-          FPRINTF(stderr, "libxsmm_shuffle: data not shuffled or copy failed!\n");
+          FPRINTF(stderr, "libxsmm_shuffle2: data not shuffled or copy failed!\n");
           result = EXIT_FAILURE; break;
         }
         /* shuffle restores initial input */
         for (i = 0; i < r; ++i) {
           memset(b, 0, size); /* clear */
-          libxsmm_shuffle(b, a, 1, s, &shuffle, NULL);
+          LIBXSMM_EXPECT(EXIT_SUCCESS == libxsmm_shuffle2(b, a, 1, s, &shuffle, NULL));
           /* every shuffle is different from input */
           if (1 < s && 0 == memcmp(a, b, s)) {
-            FPRINTF(stderr, "libxsmm_shuffle: data not shuffled!\n");
+            FPRINTF(stderr, "libxsmm_shuffle2: data not shuffled!\n");
             result = EXIT_FAILURE; break;
           }
           if (0 == memcmp(b, init, s)) break; /* restored */
           else if (r == (i + 1)) {
-            FPRINTF(stderr, "libxsmm_shuffle: data not restored!\n");
+            FPRINTF(stderr, "libxsmm_shuffle2: data not restored!\n");
             result = EXIT_FAILURE;
           }
           memcpy(a, b, s);
         }
         if (EXIT_SUCCESS == result) {
           memset(a, 0, size); /* clear */
-          libxsmm_shuffle(a, init, 1, s, &shuffle, NULL);
+          LIBXSMM_EXPECT(EXIT_SUCCESS == libxsmm_shuffle2(a, init, 1, s, &shuffle, NULL));
           memset(b, 0, size); /* clear */
-          libxsmm_shuffle(b, a, 1, s, &shuffle, &r);
+          LIBXSMM_EXPECT(EXIT_SUCCESS == libxsmm_shuffle2(b, a, 1, s, &shuffle, &r));
           if (0 != memcmp(b, init, s)) {
-            FPRINTF(stderr, "libxsmm_shuffle: data not restored!\n");
+            FPRINTF(stderr, "libxsmm_shuffle2: data not restored!\n");
             result = EXIT_FAILURE;
             break;
           }
@@ -163,7 +186,7 @@ int main(int argc, char* argv[])
         else break; /* previous error */
       }
       else {
-        FPRINTF(stderr, "libxsmm_shuffle: shuffle argument not coprime!\n");
+        FPRINTF(stderr, "libxsmm_shuffle2: shuffle argument not coprime!\n");
         result = EXIT_FAILURE;
         break;
       }

Growl1234/libxs

  - Limit warning about LIBXS_TARGET beyond CPUID to NDEBUG (verbose output), and avoid calling CPUID
    at all if LIBXS_TARGET is given. This regime allows easier debugging under ARM (no SIGILL).
  - Made libxs_get_gemm_typename use LIBXS_API_INLINE requiring no prototype,
  - Consolidated handling errors in libxs_malloc_xattrib (#775).
  - Fixed code/format conformance (libxs_get_ulp_precision).
  - Adjusted API of libxs_shuffle (libxs_memory.h).
  - More accurate LIBXS_PAGE_MINSIZE.
  - Code cleanup (libxs_coprime2).
  - Modernized equation tests to rely on libxs_matdiff_epsilon (LIBXS_MATDIFF-tool, and more robust norm, etc).
  - Modernized eltwise to rely on libxs_matdiff_epsilon (LIBXS_MATDIFF-tool, etc).
  - Introduced LIBXS_MATDIFF_SHUFFLE optionally shuffling input sequence to avoid
  - Improved math test (LIBXS_UPF).
  - Introduced LIBXS_FSSPMDM_DENSE_BIAS.
  - Enabled LIBXS_TIMER_RDTSC on AArch64, and improved portability and platform support.
  - Renamed LIBXS_CPUID_ARM_MRS to LIBXS_ARM_MRS.
  - Improved accuracy of division (libxs_timer_duration_rtc).
  - Implemented LIBXS_TIMER_RDTSC on AArch64.

tests/math.c
@@ -264,6 +264,7 @@ int main(int argc, char* argv[])
     const int n = sizeof(inp) / sizeof(*inp);
     for (i = 0; i < n; ++i) {
       if (LIBXS_UPF(inp[i], 3, 100) != out[i]) exit(EXIT_FAILURE);
+      if (LIBXS_UPF(inp[i], -100, 100) != 0) exit(EXIT_FAILURE);
     }
   }
 

tests/memory.c
@@ -116,6 +116,29 @@ int main(int argc, char* argv[])
   }
 
   if (EXIT_SUCCESS == result) { /* check libxs_shuffle */
+    char a[sizeof(init)] = { 0 }, b[sizeof(init)] = { 0 };
+    const size_t size = sizeof(init);
+    memcpy(a, init, size);
+    LIBXS_EXPECT(EXIT_SUCCESS == libxs_shuffle(a, 1, size - 1, NULL, NULL));
+    LIBXS_EXPECT(EXIT_SUCCESS == libxs_shuffle2(b, init, 1, size - 1, NULL, NULL));
+    if (0 == strcmp(a, b)) {
+      const size_t r = libxs_unshuffle(size - 1, NULL);
+      size_t i = 0;
+      for (; i < r; ++i) {
+        libxs_shuffle(a, 1, size - 1, NULL, NULL);
+      }
+      if (0 != strcmp(a, init)) {
+        FPRINTF(stderr, "libxs_shuffle: data not restored!\n");
+        result = EXIT_FAILURE;
+      }
+    }
+    else {
+      FPRINTF(stderr, "libxs_shuffle: result does not match libxs_shuffle2!\n");
+      result = EXIT_FAILURE;
+    }
+  }
+
+  if (EXIT_SUCCESS == result) { /* check libxs_shuffle2 */
     char a[sizeof(init)], b[sizeof(init)];
     const size_t size = sizeof(init);
     size_t s = 0, i;
@@ -126,35 +149,35 @@ int main(int argc, char* argv[])
         const size_t r = libxs_unshuffle(s, &shuffle);
         int cmp;
         memset(a, 0, size); /* clear */
-        libxs_shuffle(a, init, 1, s, &shuffle, NULL);
+        LIBXS_EXPECT(EXIT_SUCCESS == libxs_shuffle2(a, init, 1, s, &shuffle, NULL));
         cmp = memcmp(a, init, s);
         if ((1 >= s || 0 == cmp) && (1 < s || 0 != cmp)) {
-          FPRINTF(stderr, "libxs_shuffle: data not shuffled or copy failed!\n");
+          FPRINTF(stderr, "libxs_shuffle2: data not shuffled or copy failed!\n");
           result = EXIT_FAILURE; break;
         }
         /* shuffle restores initial input */
         for (i = 0; i < r; ++i) {
           memset(b, 0, size); /* clear */
-          libxs_shuffle(b, a, 1, s, &shuffle, NULL);
+          LIBXS_EXPECT(EXIT_SUCCESS == libxs_shuffle2(b, a, 1, s, &shuffle, NULL));
           /* every shuffle is different from input */
           if (1 < s && 0 == memcmp(a, b, s)) {
-            FPRINTF(stderr, "libxs_shuffle: data not shuffled!\n");
+            FPRINTF(stderr, "libxs_shuffle2: data not shuffled!\n");
             result = EXIT_FAILURE; break;
           }
           if (0 == memcmp(b, init, s)) break; /* restored */
           else if (r == (i + 1)) {
-            FPRINTF(stderr, "libxs_shuffle: data not restored!\n");
+            FPRINTF(stderr, "libxs_shuffle2: data not restored!\n");
             result = EXIT_FAILURE;
           }
           memcpy(a, b, s);
         }
         if (EXIT_SUCCESS == result) {
           memset(a, 0, size); /* clear */
-          libxs_shuffle(a, init, 1, s, &shuffle, NULL);
+          LIBXS_EXPECT(EXIT_SUCCESS == libxs_shuffle2(a, init, 1, s, &shuffle, NULL));
           memset(b, 0, size); /* clear */
-          libxs_shuffle(b, a, 1, s, &shuffle, &r);
+          LIBXS_EXPECT(EXIT_SUCCESS == libxs_shuffle2(b, a, 1, s, &shuffle, &r));
           if (0 != memcmp(b, init, s)) {
-            FPRINTF(stderr, "libxs_shuffle: data not restored!\n");
+            FPRINTF(stderr, "libxs_shuffle2: data not restored!\n");
             result = EXIT_FAILURE;
             break;
           }
@@ -162,7 +185,7 @@ int main(int argc, char* argv[])
         else break; /* previous error */
       }
       else {
-        FPRINTF(stderr, "libxs_shuffle: shuffle argument not coprime!\n");
+        FPRINTF(stderr, "libxs_shuffle2: shuffle argument not coprime!\n");
         result = EXIT_FAILURE;
         break;
       }