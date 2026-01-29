cd7e296f331d6b46104a68d2a156f0799cbe9f23
Hans Pabst <hans.pabst@intel.com>
2026-01-29 09:34:42 +0100

    Removed functionality out of scope and restructured accordingly

--- samples/sync/sync_lock.c
+++ samples/sync/sync_lock.c
@@ -7,7 +7,7 @@
 * Further information: https://github.com/hfp/libxs/                          *
 * SPDX-License-Identifier: BSD-3-Clause                                       *
 ******************************************************************************/
-#include <utils/libxs_timer.h>
+#include <libxs_timer.h>
 #include <libxs.h>
 
 #if defined(_OPENMP)
