705a3bb707ca27b33a27284a9b575773f97be391
Hans Pabst <hans.pabst@intel.com>
2026-02-09 11:11:48 +0100

    Prepared wrapper sample code

---
+++ samples/ozaki/ozaki.c
@@ -0,0 +1,10 @@
+/******************************************************************************
+* Copyright (c) 2009-2026 Hans Pabst                                          *
+* Copyright (c) 2009-2026 Intel Corporation                                   *
+* This file is part of the LIBXS library.                                     *
+*                                                                             *
+* For information on the license, see the LICENSE file.                       *
+* Further information: https://github.com/hfp/libxs/                          *
+* SPDX-License-Identifier: BSD-3-Clause                                       *
+******************************************************************************/
+#include "gemm.h"
---
+++ samples/ozaki/wrap.c
@@ -0,0 +1,12 @@
+/******************************************************************************
+* Copyright (c) 2009-2026 Hans Pabst                                          *
+* Copyright (c) 2009-2026 Intel Corporation                                   *
+* This file is part of the LIBXS library.                                     *
+*                                                                             *
+* For information on the license, see the LICENSE file.                       *
+* Further information: https://github.com/hfp/libxs/                          *
+* SPDX-License-Identifier: BSD-3-Clause                                       *
+******************************************************************************/
+#include "gemm.h"
+
+gemm_function_t gemm_original;
