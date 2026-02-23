6e9d23679b57f9df7219e99a5f55fc7050938adc
Hans Pabst <hans.pabst@intel.com>
2026-02-23 10:40:54 +0100

    Leverage target attributes
    - Implemented VNNI code-path (AVX-512).

--- samples/ozaki/ozaki.c
+++ samples/ozaki/ozaki.c
@@ -73,6 +73,7 @@ LIBXS_APIVAR_PRIVATE_DEF(int gemm_ozflags);
 LIBXS_APIVAR_PRIVATE_DEF(int gemm_diff_abc);
 LIBXS_APIVAR_PRIVATE_DEF(double gemm_eps);
 LIBXS_APIVAR_PRIVATE_DEF(double gemm_rsq);
+LIBXS_APIVAR_PRIVATE_DEF(int ozaki_target_arch);
 LIBXS_TLS int gemm_dump_inhibit;
 
 
@@ -136,6 +137,7 @@ LIBXS_API_INTERN LIBXS_ATTRIBUTE_WEAK void GEMM_WRAP(const char* transa, const c
         if (0 == gemm_verbose) gemm_verbose = 1;
         gemm_rsq = atof(gemm_rsq_env);
       }
+      ozaki_target_arch = libxs_cpuid(NULL);
       LIBXS_EXPECT(EXIT_SUCCESS == atexit(print_diff_atexit));
       gemm_initialized = 1;
     }
