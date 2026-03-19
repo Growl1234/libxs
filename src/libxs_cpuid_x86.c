d694a87ed659f837b1cff758e62d33ccb127a376
Hans Pabst <hans.pabst@intel.com>
2026-03-19 12:45:04 +0100

    Extended matdiff functionality
    - Renamed libxs_matdiff_info_t to libxs_matdiff_t.
    - Renamed libxs_cpuid_info_t to libxs_cpuid_t.
    - Introduced libxs_matdiff_reduce.
    - Updated ABI specification.

--- src/libxs_cpuid_x86.c
+++ src/libxs_cpuid_x86.c
@@ -116,7 +116,7 @@ LIBXS_API_INTERN void libxs_cpuid_model(char model[], size_t* model_size)
 }
 
 
-LIBXS_API_INTERN int libxs_cpuid_x86(libxs_cpuid_info_t* info)
+LIBXS_API_INTERN int libxs_cpuid_x86(libxs_cpuid_t* info)
 {
   static int result = LIBXS_TARGET_ARCH_UNKNOWN;
 #if defined(LIBXS_PLATFORM_X86)
@@ -254,7 +254,7 @@ LIBXS_API_INTERN int libxs_cpuid_x86(libxs_cpuid_info_t* info)
 }
 
 
-LIBXS_API int libxs_cpuid(libxs_cpuid_info_t* info)
+LIBXS_API int libxs_cpuid(libxs_cpuid_t* info)
 {
 #if defined(LIBXS_PLATFORM_X86)
   return libxs_cpuid_x86(info);
